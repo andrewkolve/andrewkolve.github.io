8da9ad3a46b43a31d60a95469b8a8c004f31d8a1
andrewkolve <andrewkolve@hotmail.com>
2015-10-08 17:42:45 -0700

    Q

--- locations.js
+++ locations.js
@@ -1,1 +1,9 @@
-a(["in stealth mode in Vegas","test message from phine","testMessage","in stealth mode in Vegas", "Not in Vegas"]);
+a(
+  [
+    "in stealth mode in Vegas",
+    "test message from phine",
+    "testMessage",
+    "in stealth mode in Vegas",
+    "Not in Vegas"
+  ]
+);
