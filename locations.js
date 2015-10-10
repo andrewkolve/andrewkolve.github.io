b154443b55727c48f37f17aaf6e95425ae6fbf03
andrewkolve <andrewkolve@hotmail.com>
2015-10-10 11:18:48 -0700

    Q

--- locations.js
+++ locations.js
@@ -1,5 +1,6 @@
 a(
   [
+    "Skipping pool - getting started on the day",
     "The Andrew is still sleeping things off. Come to the penthouse if interested in hanging out. Feel free to head over to the pool; we shall be fashionably late.",
     "Sleeping it off",
     "Assembling in the Ritz PENTHOUSE: rm 3355 to depart for fremont at 10pm",
